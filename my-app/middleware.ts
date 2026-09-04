56106bd820e88e4ddd57013f203debf1d5381bc6
Chandrakant Chauhan <chandrakantcsr@gmail.com>
2026-09-05 01:28:57 +0530

    fix: revert middleware config

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -3,7 +3,6 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  runtime: "nodejs",
   matcher: [
     // Run on all routes except Next.js internals and static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
